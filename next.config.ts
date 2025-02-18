5764103bbc56d4ee2b8a6c4c06ad1962ff54b1ae
highlander <bithighlander@gmail.com>
2025-02-18 14:58:44 -0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack: (config) => {
+        config.ignoreWarnings = [
+            { message: /Serializing big strings/ },
+        ];
+        return config;
+    },
 };
 
 export default nextConfig;
